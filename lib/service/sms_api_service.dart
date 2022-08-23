6cfed13a240a9e84eaba2d166ca3276c94b807b9
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-08-23 07:10:06 +0000

    added tech express sms base url

--- lib/service/sms_api_service.dart
+++ lib/service/sms_api_service.dart
@@ -11,13 +11,24 @@ class SmsApiService {
     _dio = Dio();
   }
 
-  Future sendSmsToUser(String phoneNumber) async {
+  Future sendSmsToUser(
+      {required String phoneNumber,
+      required String name,
+      required String ticketId,
+      required double price}) async {
     late String res;
     if (phoneNumber.startsWith('0')) {
       res = phoneNumber.substring(1).padLeft(13, '+233');
     } else {
       res = phoneNumber;
     }
-    _dio.post('$sms_api_base_url/booked', data: {'phoneNumber': res});
+    try {
+      _dio.post('$sms_api_base_url/booked', data: {
+        'phoneNumber': res,
+        'name': name,
+        'price': price.toString(),
+        'ticketId': ticketId
+      });
+    } on Exception {}
   }
 }
